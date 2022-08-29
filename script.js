c5eb42b832279fdb83f9d39f47194fd757226339
cqb13 <cqb13.main@gmail.com>
2022-08-29 17:39:04 -0400

    code clean up

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function setBackground(url) {
-  const backgroundEl = document.querySelector('.background');
+  const backgroundEl = document.querySelector(".background");
   backgroundEl.style.backgroundImage = `url('${url}')`;
 }
 
 function onFileSelect(e) {
   const file = e.target.files[0];
   const reader = new FileReader();
-  reader.addEventListener('load', () => {
+  reader.addEventListener("load", () => {
     try {
       localStorage.backgroundData = reader.result;
       setBackground(localStorage.backgroundData);
     } catch (err) {
-      alert('Image size must be smaller than 5MB');
+      alert("Image size must be smaller than 5MB");
     }
   });
   reader.readAsDataURL(file);
 }
 
 function listenPick() {
-  const picker = document.querySelector('.picker');
-  picker.addEventListener('change', (e)=> {
+  const picker = document.querySelector(".picker");
+  picker.addEventListener("change", (e) => {
     onFileSelect(e);
   });
 }
 
 function init() {
   listenPick();
-  if(localStorage.backgroundData) {
+  if (localStorage.backgroundData) {
     setBackground(localStorage.backgroundData);
   } else {
-    setBackground('default.png');
+    setBackground("default.png");
   }
 }
 
